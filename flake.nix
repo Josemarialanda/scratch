1963796234da3371cf4f5d683fc3f253b9b44d99
josemarialanda <josemaria.landa@gmail.com>
2023-03-18 20:38:20 +0000

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Haskell scratchpad";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = inputs:
+    let
+      overlay = final: prev: {
+        haskell = prev.haskell // {
+          packageOverrides = hfinal: hprev:
+            prev.haskell.packageOverrides hfinal hprev // {
+              scratch = hfinal.callCabal2nix "scratch" ./. { };
+            };
+        };
+        scratch = final.haskell.lib.compose.justStaticExecutables final.haskellPackages.scratch;
+      };
+      perSystem = system:
+        let
+          pkgs = import inputs.nixpkgs { inherit system; overlays = [ overlay ]; };
+          hspkgs = pkgs.haskellPackages;
+        in
+        {
+          devShell = hspkgs.shellFor {
+            withHoogle = true;
+            packages = p: [ p.scratch ];
+            buildInputs = [
+              hspkgs.cabal-install
+              hspkgs.haskell-language-server
+              hspkgs.hlint
+              hspkgs.ormolu
+              pkgs.bashInteractive
+            ];
+          };
+          defaultPackage = pkgs.scratch;
+        };
+    in
+    { inherit overlay; } // 
+      inputs.flake-utils.lib.eachDefaultSystem perSystem;
+}
